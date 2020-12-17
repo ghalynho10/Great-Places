1633d762980b0c8ebe5551e4d22969156b78cc6f
ghalynho10 <mghalynho@gmail.com>
2020-12-16 23:57:53 -0500

    first screens

---
+++ screens/PlacesListScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const PlacesListScreen = (props) => {
+  return (
+    <View>
+      <Text>PlacesListScreen</Text>
+    </View>
+  );
+};
+
+export default PlacesListScreen;
+
+const styles = StyleSheet.create({});
