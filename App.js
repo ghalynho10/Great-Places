9e3018e07c159e1757c850ddea11677f8904d5d7
ghalynho10 <mghalynho@gmail.com>
2020-12-18 17:28:48 -0500

    created local database (sqlite)

--- App.js
+++ App.js
@@ -6,6 +6,16 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import ReduxThunk from "redux-thunk";
 import placesReducer from "./store/reducers/places";
+import { init } from "./helpers/db";
+
+init()
+  .then(() => {
+    console.log("initialized database");
+  })
+  .catch((err) => {
+    console.log("Initializing database failed");
+    console.log(err);
+  });
 
 const rootReducer = combineReducers({
   places: placesReducer,
