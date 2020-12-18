171056a25914188b2a238991319ff6e3fe28741b
ghalynho10 <mghalynho@gmail.com>
2020-12-18 17:46:23 -0500

    stored data in local database

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,5 +1,6 @@
 import * as FileSystem from "expo-file-system";
 export const ADD_PLACE = "ADD_PLACE";
+import { insertPlace } from "../../helpers/db";
 
 export const addPlace = (title, image) => {
   return async (dispatch) => {
@@ -11,17 +12,25 @@ export const addPlace = (title, image) => {
         from: image,
         to: newPath,
       });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        "Dummy address",
+        15.6,
+        12.3
+      );
+      console.log(dbResult);
+      dispatch({
+        type: ADD_PLACE,
+        placeData: {
+          id: dbResult.insertId.toString(),
+          title: title,
+          image: newPath,
+        },
+      });
     } catch (error) {
       console.log(error);
       throw error;
     }
-
-    dispatch({
-      type: ADD_PLACE,
-      placeData: {
-        title: title,
-        image: newPath,
-      },
-    });
   };
 };
